0a3a14104bef97318aaa5babfed253cb4ff7e6f0
Péter Podoski <hunpody@users.noreply.github.com>
2008-09-27 11:25:32 +0000

    The renamed-and-added VariableMap.cs was missing.

---
+++ VapeTeam.Psimulex/VapeTeam.Psimulex.Core/VariableMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VapeTeam.Psimulex.Core.Types;
+
+namespace VapeTeam.Psimulex.Core
+{
+    /// <summary>
+    /// The VariableMap maps strings to basetypes in order to find variables by name. 
+    /// Useful for global variables and registers.
+    /// </summary>
+    public class VariableMap : Dictionary<string, BaseType>
+    {
+    }
+}
