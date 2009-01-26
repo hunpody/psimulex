a7983cc50d81d836bb1afd127845ec05f4d5caee
hunpody <hunpody@35c1c1a8-6353-0410-830c-475bc3951055>
2009-01-26 13:39:30 +0000

    Memory was temporarily removed from the runtime. There was a bug with bubblesort that caused "nullreference" like error in the compare operation. The buggy code is wrapped in a test called BubbleSort_01, which will pass while the memory is not fixed.

--- trunk/VapeTeam.Psimulex/VapeTeam.Psimulex.Core/Memory.cs
+++ trunk/VapeTeam.Psimulex/VapeTeam.Psimulex.Core/Memory.cs
@@ -288,6 +288,7 @@ namespace VapeTeam.Psimulex.Core
         /// <param name="baseType"></param>
         public virtual void Allocate(VapeTeam.Psimulex.Core.Types.BaseType baseType)
         {
+            return; // Temporary
             if (referenceCounter.ContainsKey(baseType.MemoryAddress))
                 return;
             int pos = 0;
@@ -334,6 +335,7 @@ namespace VapeTeam.Psimulex.Core
         /// <param name="baseType"></param>
         public virtual void DeAllocate(BaseType baseType)
         {
+            return; // Temporary
             int refCount = 0;
             if (referenceCounter.TryGetValue(baseType.MemoryAddress, out refCount))
             {
@@ -396,6 +398,7 @@ namespace VapeTeam.Psimulex.Core
         /// <param name="value"></param>
         public virtual void ReAllocate(BaseType value)
         {
+            return;
             if (!_allocationSizeInfo.ContainsKey(value.MemoryAddress))
             {
                 throw new VapeTeam.Psimulex.Core.Exceptions.MemoryException("Trying to reallocate a value that has been freed or not allocated yet.");
