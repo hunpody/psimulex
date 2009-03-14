b00af5c4859e7d8bc93438f933ad435a244cc09d
varazslo486 <varazslo486@35c1c1a8-6353-0410-830c-475bc3951055>
2009-03-14 13:53:00 +0000

    From grammer file removed the Simple program alternative. ANTLR sources regenerated. Tests wher nesessari updated with WrapInMainFunction. All test excepted 2 Passed.

--- trunk/VapeTeam.Psimulex/VapeTeam.Psimulex.Compiler.Antlr/Generated/PsimulexLexer.cs
+++ trunk/VapeTeam.Psimulex/VapeTeam.Psimulex.Compiler.Antlr/Generated/PsimulexLexer.cs
@@ -1,4 +1,4 @@
-// $ANTLR 3.1.2 C:\\Works\\Psimulex\\VapeTeam.Psimulex\\VapeTeam.Psimulex.Compiler.Antlr\\Grammar\\Psimulex.g 2009-03-14 13:32:17
+// $ANTLR 3.1.2 C:\\Works\\Psimulex\\VapeTeam.Psimulex\\VapeTeam.Psimulex.Compiler.Antlr\\Grammar\\Psimulex.g 2009-03-14 14:20:50
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 168, 219
